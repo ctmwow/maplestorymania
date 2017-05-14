45547440db130f269e1277ffeac72e4d634786cf
Fellipe Santana <fellipesac@users.noreply.github.com>
2017-05-14 17:37:49 -0300

    Correção na tradução do npc

--- emulator/scripts/npc/22000.js
+++ emulator/scripts/npc/22000.js
@@ -28,7 +28,7 @@ var status = 0;
 
 function start() 
 {
-    cm.sendYesNo("Pegue ese navil e você irá para um continente maior!. Por #e150 mesos#n, eu irei te levar para #bVictoria Island#k. A coisa é, uma vez que você deixar este lugar, você não pode nunca voltar. O que você acha? Você quer ir para Victoria Island?");
+    cm.sendYesNo("Pegue esse navil e você irá para um continente maior! Por #e150 mesos#n, eu irei te levar para #bIlha Victoria#k. Porém, uma vez que você deixar este lugar, você não nunca poderá voltar. O que você acha? Você quer ir para Ilha Victoria?");
 }
 
 function action(mode, type, selection) 
@@ -50,14 +50,14 @@ function action(mode, type, selection)
     if (status == 1) 
     {
         if (cm.haveItem(4031801))
-            cm.sendNext("Ok, agora dá-me 150 mesos ... Ei, o que é isso? É que uma carta de recomendação do Lucas, o chefe de Amherst? Ei, você deveria ter me dito que tinha isso. Eu, Shanks, reconheço a grandeza quando a vejo, e desde que você tenha sido recomendado por Lucas, eu vejo que você tem um grande, grande potencial como um aventureiro. De jeito nenhum eu iria carregá-lo para esta viagem!");
+            cm.sendNext("Ok, agora me dê 150 mesos ... Ei, o que é isso? É uma carta de recomendação do Lucas, o chefe de Amherst? Ei, você deveria ter me falado que tinha isso. Eu, Shanks, reconheço a grandeza quando a vejo, e desde que você tenha sido recomendado por Lucas, eu vejo que você tem um grande, grande potencial como um aventureiro. De jeito nenhum eu iria cobrá-lo por esta viagem!");
         else
-            cm.sendNext("Entediado deste lugar? Aqui... Dê-me #e150 mesos#n primeiro...");
+            cm.sendNext("Entediado deste lugar? Aqui... Me dê #e150 mesos#n primeiro...");
     }
      else if (status == 2) 
      {
         if (cm.haveItem(4031801))
-            cm.sendNextPrev("Desde que você tenha a carta de recomendação, não vou cobrar-lhe por isso. Tudo bem, apertem os cintos, porque vamos para Victoria Island, e pode ser um pouco turbulento!!");
+            cm.sendNextPrev("Desde que você tenha a carta de recomendação, não lhe cobrarei por isso. Tudo bem, apertem os cintos, porque vamos para Ilha Victoria, e pode ser um pouco turbulento!!");
         else
         if (cm.getLevel() > 6) 
         {
@@ -67,11 +67,11 @@ function action(mode, type, selection)
                 cm.dispose();
             }
              else
-                cm.sendNext("Perfeito! #e150#n mesos aceitos! Tudo bem, vamos para Victoria Island!");
+                cm.sendNext("Perfeito! #e150#n mesos aceitos! Tudo bem, vamos para Ilha Victoria!");
         } 
         else 
         {
-            cm.sendOk("Vamos ver ... Eu não acho que você é forte o suficiente. Você vai ter que ser, pelo menos, Level 7 para ir à Victoria Island.");
+            cm.sendOk("Vamos ver ... Eu não acho que você é forte o suficiente. Você vai ter que ser, pelo menos, Level 7 para ir à Ilha Victoria.");
             cm.dispose();
         }
     }
