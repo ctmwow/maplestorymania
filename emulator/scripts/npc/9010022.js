fafe58d90ce8e57f6d1c62355c590a9492e7fe03
Fellipe Santana <fellipesac@users.noreply.github.com>
2017-05-14 20:23:20 -0300

    tradução e correção no script

--- emulator/scripts/npc/9010022.js
+++ emulator/scripts/npc/9010022.js
@@ -1,53 +1,80 @@
-function start() {
-    cm.askMapSelection("#1# Mu Lung Training Center#2# Monster Carnival 1#3# Monster Carnival 2#4# Dual Raid#5# Nett's Pyramid#6# Kerning Subway#7# Happyville#8# Golden Temple#9# Crimsonwood Party Quest");
-}
+importPackage(Packages.org.ascnet.leaftown.client); 
+importPackage(Packages.org.ascnet.leaftown.server.maps); 
 
-function action(mode, type, selection) {
-    if (mode == 1) {
-		switch (selection) {
-			case 1:
-				if (cm.getPlayer().getLevel() >= 25) {
-					cm.getPlayer().saveLocation("MIRROR");
-					cm.warp(925020000, 0);
-				}
-				break;
-			case 2:
-				if (cm.getPlayer().getLevel() >= 30) {
-					cm.getPlayer().saveLocation("MIRROR"); // h4x
-					cm.warp(980000000, 4);
-				}
-				break;
-			case 3:
-				if (cm.getPlayer().getLevel() >= 51) {
-					cm.getPlayer().saveLocation("MIRROR"); // h4x
-					cm.warp(980030000, 4);
-				}
-				break;
-			case 4:
-				if (cm.getPlayer().getLevel() >= 60) {
-					cm.getPlayer().saveLocation("MIRROR"); // h4x
-					cm.warp(923020000, 0);
-				}
-				break;
-			case 5:
-				if (cm.getPlayer().getLevel() >= 40) {
-					cm.getPlayer().saveLocation("MIRROR"); // h4x
-					cm.warp(926010000, 4);
-				}
-				break;
-			case 6:
-				cm.getPlayer().saveLocation("MIRROR"); // h4x
-				cm.warp(209000000, 0);
-				break;
-			case 7:
-				cm.getPlayer().saveLocation("MIRROR"); // h4x
-				cm.warp(950100000, 9);
-				break;
-			case 8:
-				cm.getPlayer().saveLocation("MIRROR"); // h4x
-				cm.warp(610010000, 0);
-			break;
-		}
+var status; 
+var sel; 
+
+function start() { 
+    status = -1; 
+    action(1, 0, 0); 
+} 
+
+function action(mode, type, selection) { 
+    if (mode == -1) { 
+        cm.dispose(); 
+    } else { 
+        if (mode == 0) { 
+            cm.dispose(); 
+            return; 
+        } 
+        if (mode == 1) 
+            status++; 
+        else 
+            status--; 
+            if (status == 0) { 
+            if (cm.getLevel() < 20) { 
+                cm.askMapSelection("#-1# Não há lugar para você transportar a partir daqui."); 
+                cm.dispose(); 
+            } else { 
+                var selStr = ""; 
+                if (cm.getLevel() >= 20 && cm.getLevel() <= 30) { 
+                    selStr += "#0# Ariant Coliseum"; 
+                } 
+
+                if (cm.getLevel() >= 25) { 
+                    selStr += "#1# Mu Lung Dojo"; 
+                } 
+
+                if (cm.getLevel() >= 30 && cm.getLevel() <= 50) { 
+                    selStr += "#2# Monster Carnival 1"; 
+                } 
+
+                if (cm.getLevel() >= 51 && cm.getLevel() <= 70) { 
+                    selStr += "#3# Monster Carnival 2"; 
+                } 
+
+                if (cm.getLevel() >= 40) { 
+                    selStr += "#5# Nett's Pyramid"; 
+                } 
+
+                if (cm.getLevel() >= 25 && cm.getLevel() <= 30) { 
+                    selStr += "#6# Construction Site"; 
+                } 
+                cm.askMapSelection(selStr); 
+            } 
+        } else if (status == 1) { 
+            cm.getPlayer().saveLocation("MIRROR"); 
+            switch (selection) { 
+                case 0: 
+                    cm.warp(980010000, 3); 
+                    break; 
+                case 1: 
+                    cm.warp(925020000); 
+                    break; 
+                case 2: 
+                    cm.warp(980000000, 3); 
+                    break; 
+                case 3: 
+                    cm.warp(980030000, 3); 
+                    break; 
+                case 5: 
+                    cm.warp(926010000); 
+                    break; 
+                case 6: 
+                    cm.warp(910320000); 
+                    break; 
+            } 
+            cm.dispose(); 
+        } 
     }
-    cm.dispose();
-}
+}	
