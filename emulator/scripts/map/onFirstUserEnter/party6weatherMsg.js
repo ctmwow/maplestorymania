d51eb954d65f57351efb626f77396fd1f0a35ecf
Fellipe Santana <fellipesac@users.noreply.github.com>
2017-05-14 19:24:30 -0300

    tradução do script

--- emulator/scripts/map/onFirstUserEnter/party6weatherMsg.js
+++ emulator/scripts/map/onFirstUserEnter/party6weatherMsg.js
@@ -1,25 +1,25 @@
 function start(ms) {
     switch (ms.getPlayer().getMapId()) { 
 		case 930000000: 
-			ms.getPlayer().getMap().startMapEffect("Step in the portal to be transformed.", 5120023); 
+			ms.getPlayer().getMap().startMapEffect("Entre no portal para ser transformado.", 5120023); 
 			break; 
 		case 930000100: 
-			ms.getPlayer().getMap().startMapEffect("Defeat the poisoned monsters!", 5120023); 
+			ms.getPlayer().getMap().startMapEffect("Derrote os monstros envenenados!", 5120023); 
 			break; 
 		case 930000200: 
-			ms.getPlayer().getMap().startMapEffect("Eliminate the spore that blocks the way by purifying the poison!", 5120023); 
+			ms.getPlayer().getMap().startMapEffect("Elimine o esporo que bloqueia o caminho purificando o veneno!", 5120023); 
 			break; 
 		case 930000300: 
-			ms.getPlayer().getMap().startMapEffect("Uh oh! The forest is too confusing! Find me, quick!", 5120023); 
+			ms.getPlayer().getMap().startMapEffect("Aaaah! A floresta é muito confusa! Encontre-me rápido!", 5120023); 
 			break; 
 		case 930000400: 
-			ms.getPlayer().getMap().startMapEffect("Purify the monsters by getting Purification Marbles from me!", 5120023); 
+			ms.getPlayer().getMap().startMapEffect("Purifique os monstros obtendo Mármore de Monstro para mim!", 5120023); 
 			break; 
 		case 930000500: 
-			ms.getPlayer().getMap().startMapEffect("Find the Purple Magic Stone!", 5120023); 
+			ms.getPlayer().getMap().startMapEffect("Encontre a Pedra Mágica Roxa!", 5120023); 
 			break; 
 		case 930000600: 
-			ms.getPlayer().getMap().startMapEffect("Place the Magic Stone on the altar!", 5120023); 
+			ms.getPlayer().getMap().startMapEffect("Coloque a Pedra Mágica no altar!", 5120023); 
 			break; 
 	} 
 }
